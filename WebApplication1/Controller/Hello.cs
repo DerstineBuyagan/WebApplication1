f111128b3e667ca895ddb6dbb833a0d1c5515f5b
Derstine Buyagan <bastedboyderzthine@gmail.com>
2018-11-11 03:29:56 +0800

    Add project files.

---
+++ WebApplication1/Controller/Hello.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApi.Controller
+{
+
+    public class HelloController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        //We need this for routing
+        [HttpGet("api/demo")]
+        public JsonResult Demo()
+        {
+            return new JsonResult(new List<PersonDto>
+            {
+                 new PersonDto
+                 {
+                     FirstName = "Derstine",
+                     LasteName = "Derstine",
+                 },
+                new PersonDto
+                 {
+                     FirstName = "Denden",
+                     LasteName = "Buyagan",
+                 },
+            }
+            );
+        }
+    }
+
+    public class PersonDto
+    {
+        public string FirstName { get; set; }
+        public string LasteName { get; set; }
+    }
+}
---
+++ WebApplication1/Controller/Person.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controller
+{
+    public class Person:Microsoft.AspNetCore.Mvc.Controller
+    {
+        [HttpGet("api/Person")]
+        public JsonResult PersonName()
+        {
+            return  new JsonResult("Person");
+        }
+    }
+}
